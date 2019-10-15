651c2478122b800ea36e9a3329ab7513d42442f4
Wendy Harris <wendyv510@hotmail.com>
2019-10-15 21:17:23 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,5 +4,9 @@ def batch_badge_creator=["Edsger","Ada", "Charles", "Alan", "Grace", "Linus", "M
   end 
   
   def assign_rooms=[1,2,3,4,5,6,7](batch_badge_creator)
+    assign_rooms.each do |room number|
+      puts "Hello, #{name}! You'll be assigned to room #{room number}!"
+    end 
+    
     
     
